c43596d9a3f4880d5fa7cb10fc631f9e69b2fdd5
Ruben Giannotti <ruben.giannotti@gmx.net>
2019-01-13 18:02:04 +0100

    Add files via upload

---
+++ build/precache-manifest.2d225adfe4ae333463e1aa3cb409176a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bff7b953eeea43028e2e4b0ee4657f8",
+    "url": "/static/media/vonfreunden-details.6bff7b95.png"
+  },
+  {
+    "revision": "513cfaa321441a5545f9",
+    "url": "/static/css/main.adc81719.chunk.css"
+  },
+  {
+    "revision": "bb95dbf5ea0f3a34930d8817436f6c89",
+    "url": "/static/media/partitur.bb95dbf5.jpg"
+  },
+  {
+    "revision": "8e7fd4d9cd9460493e32",
+    "url": "/static/js/1.8e7fd4d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e9580363b58bdfa3bc31e2fe7c3993f",
+    "url": "/static/media/logo.9e958036.svg"
+  },
+  {
+    "revision": "e167e975cacab22ba5942553fc9e13d1",
+    "url": "/static/media/nav-iconset.e167e975.svg"
+  },
+  {
+    "revision": "088957efc416fd0e782d112b25175c10",
+    "url": "/static/media/nogreaterlove-shelf.088957ef.png"
+  },
+  {
+    "revision": "02c2ada6068ca6f047b5481a37a59866",
+    "url": "/static/media/nogreaterlove-details.02c2ada6.png"
+  },
+  {
+    "revision": "9626dfa1f7492684968705098f9b7eca",
+    "url": "/static/media/vonfreunden-shelf.9626dfa1.png"
+  },
+  {
+    "revision": "513cfaa321441a5545f9",
+    "url": "/static/js/main.513cfaa3.chunk.js"
+  },
+  {
+    "revision": "45abf170e5eb1dd27fd0d22808c38a74",
+    "url": "/static/media/planeten-shelf.45abf170.png"
+  },
+  {
+    "revision": "9cd50bf7b5f0168037785bcdfbd4b101",
+    "url": "/static/media/planeten-details.9cd50bf7.png"
+  },
+  {
+    "revision": "6047ce3d1860f3a195c77f4837f8de0f",
+    "url": "/static/media/vaermeland-shelf.6047ce3d.png"
+  },
+  {
+    "revision": "f1d87e8adfdfd886800a13b96ef4c56e",
+    "url": "/static/media/vaermeland-details.f1d87e8a.png"
+  },
+  {
+    "revision": "8401a9129571250bca2f80973f1ff1a0",
+    "url": "/static/media/sleepin_beecroft-shelf.8401a912.png"
+  },
+  {
+    "revision": "1538146cbe51cf860137afa3d0b9b2e9",
+    "url": "/static/media/sleepin_beecroft-details.1538146c.png"
+  },
+  {
+    "revision": "075b96681ae5da3fbc15dac2207ae257",
+    "url": "/static/media/stoa_bridge.075b9668.jpg"
+  },
+  {
+    "revision": "8e7fd4d9cd9460493e32",
+    "url": "/static/css/1.ddf7f947.chunk.css"
+  },
+  {
+    "revision": "0d229a12fa7d32d467dca1fa90b250c0",
+    "url": "/index.html"
+  }
+];
